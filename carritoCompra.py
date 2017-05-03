a05767b66bbd3d844ebbc3b8ee357e15e55c0507
katiavega <katia.canepa@gmail.com>
2017-05-03 16:05:13 -0500

    nuevo archivo de ejemplo

---
+++ carritoCompra.py
@@ -0,0 +1,9 @@
+articulo1 = 10
+articulo2 = 40
+
+def totalizarCompra(item1, item2):
+   costoTotal = item1 + item2
+   return costoTotal
+
+total1 = totalizarCompra(articulo1, articulo2)
+print (total1)
